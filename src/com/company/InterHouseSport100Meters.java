56c8b7b1a8da15f88d15327b62b83f16ee7eb8e9
Eugenex <eugene@sankore.com>
2022-03-24 22:28:51 +0100

    init Commit

--- src/com/company/InterHouseSport100Meters.java
+++ src/com/company/InterHouseSport100Meters.java
@@ -1,11 +1,11 @@
 package com.company;
 
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class InterHouseSport100Meters {
 
-    private static Object lock = new Object();
-
+    private static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
         Thread t1 = new Thread(new InterHouseSport100Meter(ClubColors.ANSI_YELLOW), "Priority 1");
@@ -13,15 +13,16 @@ public class InterHouseSport100Meters {
         Thread t3 = new Thread(new InterHouseSport100Meter(ClubColors.ANSI_GREEN), "Priority 67");
         Thread t4 = new Thread(new InterHouseSport100Meter(ClubColors.ANSI_RED), "Priority 4");
 
+        t1.setPriority(10);
+        t1.setPriority(8);
+        t1.setPriority(5);
+        t1.setPriority(1);
 
         System.out.println("100 meters competion \n10 = 10Meters");
         t3.start();
         t2.start();
         t4.start();
         t1.start();
-
-
-
     }
 
 
@@ -37,19 +38,17 @@ public class InterHouseSport100Meters {
         public void run() {
             Random random = new Random();
             for (int i = 0; i < 10; i++) {
-
-                synchronized (lock) {
-                    System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName() , runCount++);
+                lock.lock();
                     try {
+                        System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName() , runCount++);
                         Thread.sleep(random.nextInt(1000));
                     } catch (InterruptedException e) {
                         System.out.println("This announcement  was interrupted");
                         System.out.println("Who will win the Interhouse sport 100m race");
-                    }
+                    }finally{
+                        lock.unlock();}
                     // execute critial section of code
                 }
             }
         }
     }
-
-}
